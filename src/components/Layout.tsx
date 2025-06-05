72846be34141ea8c36c62e6788548a0cfb45f76d
D. Moschokarfi <despinamx16@gmail.com>
2025-06-05 14:30:04 +0300

    feat: Responsive header added on Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,18 @@
-// import CodingFactoryLogo from "./CodingFactoryLogo.tsx";
-import Header from "./Header.tsx";
+import React from "react";
+// import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
+import HeaderResponsive from "./HeaderResponsive.tsx";
 
 interface LayoutProps{
-    children?: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const Layout = ({children}:LayoutProps) => {
     return (
         <>
-            <Header/>
-            <div className="container mx-auto min-h-[90vh] pt-24">
+            {/*<Header/>*/}
+            <HeaderResponsive/>
+            <div className="container mx-auto min-h-[95vh] pt-24">
                 {children}
             </div>
             <Footer/>
