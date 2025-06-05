5ab7950c908680d794cc64ff7f665ffa5ee05c6c
D. Moschokarfi <despinamx16@gmail.com>
2025-06-05 14:28:37 +0300

    feat: New page added for home

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import {useEffect} from "react";
+
+const HomePage = () => {
+
+    useEffect(() => {
+        document.title = 'HomePage';
+    }, [])
+    return (
+        <>
+            <h1 className="text-bold text-2xl text-center mt-8">Home Page</h1>
+        </>
+    )
+};
+export default HomePage;
