ea0bda5ecbae4940380336109b62d29a5bafb50e
D. Moschokarfi <despinamx16@gmail.com>
2025-06-11 12:36:06 +0300

    feat: Focus Input component created with useRef

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import ExamplesPage from "./pages/ExamplesPage.tsx";
 import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
 import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
+import FocusInput from "./components/FocusInput.tsx";
 
 function App() {
 
@@ -31,7 +32,8 @@ function App() {
 
                     <Route element={<RouterLayout/>}>
                         {/*<Route path="/" element={<HomePage />}/>*/}
-                        <Route index element={<HomePage/>}/>
+                        {/*<Route index element={<HomePage/>}/>*/}
+                        <Route index element={<FocusInput/>}/>
                         <Route path="users/:userId" element={<UserPage />}/>
                         <Route path="users" element={<UserPage/>}/>
                         <Route path="*" element={<NotFoundPage/>}/>
