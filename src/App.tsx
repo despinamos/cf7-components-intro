7e18b9388cc34d4f0618cefd228a356a2a2962e8
D. Moschokarfi <despinamx16@gmail.com>
2025-06-12 08:52:23 +0300

    feat: MultiFieldForm component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import {BrowserRouter, Routes, Route} from "react-router"
-//import Layout from "./components/Layout"
-//import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import RouterLayout from "./components/RouterLayout.tsx";
-import ExamplesPage from "./pages/ExamplesPage.tsx";
 import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
 import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
-//import FocusInput from "./components/FocusInput.tsx";
+import FocusInput from "./components/FocusInput.tsx";
 import UncontrolledInput from "./components/UncontrolledInput.tsx";
+import MultiFieldForm from "./components/MultiFieldForm.tsx";
+import ControlledInput from "./components/ControlledInput.tsx";
+import ExamplesPage from "./pages/ExamplesPage.tsx";
 
 function App() {
 
@@ -29,12 +29,15 @@ function App() {
                         <Route path="name-changer" element={<NameChangerPage/>}/>
                         <Route path="online-status" element={<OnlineStatusPage/>}/>
                         <Route path="auto-redirect" element={<AutoRedirectPage/>}/>
+                        <Route path="controlled-input" element={<ControlledInput/>}/>
+                        <Route path="uncontrolled-input" element={<UncontrolledInput/>}/>
+                        <Route path="focus-input" element={<FocusInput/>}/>
                     </Route>
 
                     <Route element={<RouterLayout/>}>
                         {/*<Route path="/" element={<HomePage />}/>*/}
                         {/*<Route index element={<HomePage/>}/>*/}
-                        <Route index element={<UncontrolledInput/>}/>
+                        <Route index element={<MultiFieldForm/>}/>
                         <Route path="users/:userId" element={<UserPage />}/>
                         <Route path="users" element={<UserPage/>}/>
                         <Route path="*" element={<NotFoundPage/>}/>
